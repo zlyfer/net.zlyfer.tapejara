038d289026ad351607f9b087b2971cdb00d28237
Frederik Shull <zlyfer@pm.me>
2024-06-25 22:46:46 +0200

    android added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'net.zlyfer.pteroapp',
   appName: 'PteroApp',
   webDir: 'www'
 };
